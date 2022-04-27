15797bf97ad6a0fa16bbcd262f7848840f87d9d8
MaxSantana <maxsantana92ok@gmail.com>
2022-04-27 20:12:14 -0300

    add text header

--- code-challenge-react/src/Components/organisms/Header/Header.jsx
+++ code-challenge-react/src/Components/organisms/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <React.Fragment>
       <div className="CP-Headers element-shadow animate__animated animate__fadeIn">
-        <h2 className="primary-color">Cultura It - Blog</h2>
+        <h2 className="primary-color">Cultura It - Blog - Challenge</h2>
       </div>
     </React.Fragment>
   );
